200ef0c9a1861fd9622a15b62429f94a7b4dcfb0
Shantanu Deshpande <mr.shankardeshpande@gmail.com>
2024-10-05 12:31:06 +0530

    style changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -87,7 +87,7 @@ function Signup() {
                             <input
                                 type="email"
                                 id="email-address-icon"
-                                placeholder="name@flowbite.com"
+                                placeholder="name@example.com"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 {...register('email', {
                                     required: 'Email is required',
